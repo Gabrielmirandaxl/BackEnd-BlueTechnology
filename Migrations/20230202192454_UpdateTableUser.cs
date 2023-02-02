1b6e4b7dcd27c891489d2a6c64125a175b403466
Gabriel Miranda <gabrielmirandaxl109@gmail.com>
2023-02-02 16:25:46 -0300

    feat: adding update in table users

---
+++ Migrations/20230202192454_UpdateTableUser.cs
@@ -0,0 +1,226 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace crud.Migrations
+{
+    /// <inheritdoc />
+    public partial class UpdateTableUser : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_Users",
+                table: "Users");
+
+            migrationBuilder.RenameTable(
+                name: "Users",
+                newName: "tb_users");
+
+            migrationBuilder.RenameColumn(
+                name: "UpdateRegistration",
+                table: "tb_users",
+                newName: "updateRegistration");
+
+            migrationBuilder.RenameColumn(
+                name: "Telefone",
+                table: "tb_users",
+                newName: "telefone");
+
+            migrationBuilder.RenameColumn(
+                name: "Name",
+                table: "tb_users",
+                newName: "name");
+
+            migrationBuilder.RenameColumn(
+                name: "Email",
+                table: "tb_users",
+                newName: "email");
+
+            migrationBuilder.RenameColumn(
+                name: "CreateRegistration",
+                table: "tb_users",
+                newName: "createRegistration");
+
+            migrationBuilder.RenameColumn(
+                name: "Cpf",
+                table: "tb_users",
+                newName: "cpf");
+
+            migrationBuilder.RenameColumn(
+                name: "Id",
+                table: "tb_users",
+                newName: "id");
+
+            migrationBuilder.UpdateData(
+                table: "tb_users",
+                keyColumn: "telefone",
+                keyValue: null,
+                column: "telefone",
+                value: "");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "telefone",
+                table: "tb_users",
+                type: "longtext",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "longtext",
+                oldNullable: true)
+                .Annotation("MySql:CharSet", "utf8mb4")
+                .OldAnnotation("MySql:CharSet", "utf8mb4");
+
+            migrationBuilder.UpdateData(
+                table: "tb_users",
+                keyColumn: "name",
+                keyValue: null,
+                column: "name",
+                value: "");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "name",
+                table: "tb_users",
+                type: "longtext",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "longtext",
+                oldNullable: true)
+                .Annotation("MySql:CharSet", "utf8mb4")
+                .OldAnnotation("MySql:CharSet", "utf8mb4");
+
+            migrationBuilder.UpdateData(
+                table: "tb_users",
+                keyColumn: "email",
+                keyValue: null,
+                column: "email",
+                value: "");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "email",
+                table: "tb_users",
+                type: "longtext",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "longtext",
+                oldNullable: true)
+                .Annotation("MySql:CharSet", "utf8mb4")
+                .OldAnnotation("MySql:CharSet", "utf8mb4");
+
+            migrationBuilder.UpdateData(
+                table: "tb_users",
+                keyColumn: "cpf",
+                keyValue: null,
+                column: "cpf",
+                value: "");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "cpf",
+                table: "tb_users",
+                type: "longtext",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "longtext",
+                oldNullable: true)
+                .Annotation("MySql:CharSet", "utf8mb4")
+                .OldAnnotation("MySql:CharSet", "utf8mb4");
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_tb_users",
+                table: "tb_users",
+                column: "id");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_tb_users",
+                table: "tb_users");
+
+            migrationBuilder.RenameTable(
+                name: "tb_users",
+                newName: "Users");
+
+            migrationBuilder.RenameColumn(
+                name: "updateRegistration",
+                table: "Users",
+                newName: "UpdateRegistration");
+
+            migrationBuilder.RenameColumn(
+                name: "telefone",
+                table: "Users",
+                newName: "Telefone");
+
+            migrationBuilder.RenameColumn(
+                name: "name",
+                table: "Users",
+                newName: "Name");
+
+            migrationBuilder.RenameColumn(
+                name: "email",
+                table: "Users",
+                newName: "Email");
+
+            migrationBuilder.RenameColumn(
+                name: "createRegistration",
+                table: "Users",
+                newName: "CreateRegistration");
+
+            migrationBuilder.RenameColumn(
+                name: "cpf",
+                table: "Users",
+                newName: "Cpf");
+
+            migrationBuilder.RenameColumn(
+                name: "id",
+                table: "Users",
+                newName: "Id");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Telefone",
+                table: "Users",
+                type: "longtext",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "longtext")
+                .Annotation("MySql:CharSet", "utf8mb4")
+                .OldAnnotation("MySql:CharSet", "utf8mb4");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Name",
+                table: "Users",
+                type: "longtext",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "longtext")
+                .Annotation("MySql:CharSet", "utf8mb4")
+                .OldAnnotation("MySql:CharSet", "utf8mb4");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Email",
+                table: "Users",
+                type: "longtext",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "longtext")
+                .Annotation("MySql:CharSet", "utf8mb4")
+                .OldAnnotation("MySql:CharSet", "utf8mb4");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Cpf",
+                table: "Users",
+                type: "longtext",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "longtext")
+                .Annotation("MySql:CharSet", "utf8mb4")
+                .OldAnnotation("MySql:CharSet", "utf8mb4");
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_Users",
+                table: "Users",
+                column: "Id");
+        }
+    }
+}
