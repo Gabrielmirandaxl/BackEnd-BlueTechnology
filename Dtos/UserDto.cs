8f9032161f90a4bef2a390b81e75b6c859344c77
Gabriel Miranda <gabrielmirandaxl109@gmail.com>
2023-02-02 22:43:33 -0300

    feat: adding dtos of user

---
+++ Dtos/UserDto.cs
@@ -0,0 +1,10 @@
+namespace crud.Dtos
+{
+  public class UserDto
+  {
+    public int Id { get; set; }
+    public string? Name { get; set; }
+    public string? Telefone { get; set; }
+    public string? Email { get; set; }
+  }
+}
